db30639645807fd5f78a71687a58d17f4142efd9
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-04-22 15:07:31 -0300

    refactor: new register modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal';
 import { FiX } from 'react-icons/fi';
+import { FormEvent, useState } from 'react';
 import { Container } from './styles';
+import database from '../../services/firebase';
 
 interface NewTransactionProps {
   isOpen: boolean;
@@ -10,6 +12,33 @@ export function NewTransactionModal({
   isOpen,
   onRequestClose,
 }: NewTransactionProps) {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState(0);
+  const [cpfId, setCpfId] = useState('');
+  const [maritalStatus, setMaritalStatus] = useState('');
+  const [city, setCity] = useState('');
+  const [state, setState] = useState('');
+
+  async function handleCreateNewRegister(event: FormEvent) {
+    event.preventDefault();
+
+    database.child('registers').push({
+      name,
+      age,
+      cpfId,
+      maritalStatus,
+      city,
+      state,
+    });
+    setName('');
+    setAge(0);
+    setCpfId('');
+    setMaritalStatus('');
+    setCity('');
+    setState('');
+    onRequestClose();
+  }
+
   return (
     <Modal
       isOpen={isOpen}
@@ -25,20 +54,50 @@ export function NewTransactionModal({
         <FiX />
       </button>
 
-      <Container>
+      <Container onSubmit={handleCreateNewRegister}>
         <h2>Cadastrar nova pessoa</h2>
 
-        <input type="text" placeholder="Digite o nome" />
+        <input
+          type="text"
+          placeholder="Digite o nome"
+          value={name}
+          onChange={event => setName(event.target.value)}
+        />
 
-        <input type="text" placeholder="Digite a idade" />
+        <input
+          type="number"
+          placeholder="Digite a idade"
+          value={age}
+          onChange={event => setAge(Number(event.target.value))}
+        />
 
-        <input type="text" placeholder="Informe o CPF" />
+        <input
+          type="text"
+          placeholder="Informe o CPF"
+          value={cpfId}
+          onChange={event => setCpfId(event.target.value)}
+        />
 
-        <input type="text" placeholder="Qual o estado civil" />
+        <input
+          type="text"
+          placeholder="Qual o estado civil"
+          value={maritalStatus}
+          onChange={event => setMaritalStatus(event.target.value)}
+        />
 
-        <input type="text" placeholder="Informe a cidade" />
+        <input
+          type="text"
+          placeholder="Informe a cidade"
+          value={city}
+          onChange={event => setCity(event.target.value)}
+        />
 
-        <input type="text" placeholder="Informe o estado" />
+        <input
+          type="text"
+          placeholder="Informe o estado"
+          value={state}
+          onChange={event => setState(event.target.value)}
+        />
 
         <button type="submit">Cadastrar</button>
       </Container>
